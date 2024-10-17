0d66b8353a7eb94ed7825725cb3ab1953b561d73
aizatnazran <aizatnazran@gmail.com>
2024-10-18 03:49:57 +0800

    add port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,20 +14,14 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: '',
   },
-  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
   hooks: {
     'vite:extendConfig': function (config: any, { isServer }: any) {
       if (isServer) {
-        // Workaround for netlify issue
-        // https://github.com/nuxt/framework/issues/6204
-        config.build.rollupOptions.output.inlineDynamicImports = true
+        config.build.rollupOptions.output.inlineDynamicImports = true;
       }
     },
   },
   css: [
-    // 'primevue/resources/themes/bootstrap4-dark-blue/theme.css',
-    // 'primevue/resources/primevue.css',
-    // 'primeicons/primeicons.css',
     '/assets/css/style.css',
   ],
   build: {
@@ -52,5 +46,11 @@ export default defineNuxtConfig({
   loading: {
     color: 'blue',
     height: '5px',
+  },
+  
+  // Add this server configuration to listen on port 3001
+  server: {
+    port: 3001, // Specify port 3001
+    host: '0.0.0.0', // Listen on all network interfaces (important for Docker)
   },
 })
