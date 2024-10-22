eeb1636698743952c6452d69b52c4cae5cb4baea
aizatnazran <aizatnazran@gmail.com>
2024-10-22 16:29:24 +0800

    Remove head 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,6 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      titleTemplate: '%s - Aizat Nazran | DevOps Engineer',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
